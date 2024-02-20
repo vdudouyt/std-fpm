fb01433355266a77719b5be687ce6723e81b5593
vdudouyt <valentin.dudouyt@gmail.com>
2024-02-20 19:06:07 +0200

    Log message content

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <ctype.h>
 //#include <gmodule.h>
 
 #include "fcgi_parser.h"
@@ -157,11 +158,31 @@ void onsocketwriteok(fd_ctx_t *ctx) {
    log_write(MAINLOG, "[%s] socket is ready for writing", ctx->name);
 }
 
+static void escape(unsigned char *out, const unsigned char *in, size_t input_length) {
+   unsigned int d = 0;
+   for(unsigned int i = 0; i < input_length; i++) {
+      if(isprint(in[i])) {
+         out[d++] = in[i];
+      } else {
+         out[d++] = '\\';
+         out[d++] = 'x';
+         sprintf(&out[d], "%02x", in[i]);
+         d += 2;
+      }
+   }
+   out[d++] = '\0';
+}
+
 void onfcgimessage(const fcgi_header_t *hdr, const char *data, void *userdata) {
    fd_ctx_t *ctx = userdata;
 
    log_write(MAINLOG, "[%s] got fcgi message: { type = %s, requestId = 0x%02x, contentLength = %d }",
-      ctx->name, fcgitype_to_string(hdr->type), hdr->requestId, hdr->contentLength); 
+      ctx->name, fcgitype_to_string(hdr->type), hdr->requestId, hdr->contentLength);
+   if(hdr->contentLength) {
+      char escaped_data[4*65536+1];
+      escape(escaped_data, data, hdr->contentLength);
+      log_write(MAINLOG, "[%s] message content: \"%s\"", ctx->name, escaped_data);
+   }
 
    if(hdr->type == FCGI_PARAMS) fcgi_params_parser_write(ctx->client->params_parser, data, hdr->contentLength);
 }
