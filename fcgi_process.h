96e43fa7f0f144496996d07b464bfc1fa938aa0e
vdudouyt <valentin.dudouyt@gmail.com>
2024-03-06 15:06:21 +0200

    Removed proc->next and proc->bucket

--- fcgi_process.h
+++ fcgi_process.h
@@ -8,10 +8,7 @@ struct fcgi_process_s {
 	char filepath[4096];
 	struct sockaddr_un s_un;
    pid_t pid;
-   struct fcgi_process_s *next;
-
    int fd;
-   void *bucket;
 };
 
 fcgi_process_t *fcgi_spawn(const char *path);
