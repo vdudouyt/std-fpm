9c69305e5e54781e1e837d661ae7b81391f11b96
vdudouyt <valentin.dudouyt@gmail.com>
2024-03-08 20:42:56 +0200

    Fixed env.c example

--- examples/c/env.c
+++ examples/c/env.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <fcgi_stdio.h>
+#include <stdlib.h>
 
 // gcc env.c -lfcgi -o env.fcgi
+
+extern char **environ;
 
 int main(int argc, char **argv, char **envp) {
    while (FCGI_Accept() >= 0) {
       printf("Content-type: text/plain\n\n");
-      for (char **env = envp; *env != 0; env++) {
+
+      char **env = environ;
+      for(env; *env; env++) {
          printf("%s\n", (*env));    
       }
    }
